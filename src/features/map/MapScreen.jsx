85050c238d9cd4866e7f074b92004294fe35ce2c
Carlos Alberto Furtado <seu-email@gmail.com>
2026-09-15 15:32:14 -0300

    fix: make map retina ready

--- src/features/map/MapScreen.jsx
+++ src/features/map/MapScreen.jsx
@@ -233,6 +233,7 @@ export default function MapScreen() {
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              detectRetina={true}
             />
             <FlyToLocation location={userLocation} />
             {userLocation && (
